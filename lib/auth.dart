a0ad7438a101f15f4dc45b183f8df0e4e04d7e0f
tundak <sonu5650@gmail.com>
2019-11-21 12:46:29 +0530

    fix with latest firebase

--- lib/auth.dart
+++ lib/auth.dart
@@ -13,13 +13,13 @@ class Auth implements BaseAuth {
 
   @override
   Future<String> signInWithEmailAndPassword(String email, String password) async {
-    final FirebaseUser user = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+    final FirebaseUser user = (await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password)).user;
     return user?.uid;
   }
 
   @override
   Future<String> createUserWithEmailAndPassword(String email, String password) async {
-    final FirebaseUser user = await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+    final FirebaseUser user = (await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password)).user;
     return user?.uid;
   }
 
